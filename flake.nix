3c96dac44a7a22ffbab09f35fcb6bf30238cc6b9
borazan <boratuncel@gmail.com>
2024-02-26 01:36:40 +0100

    syntax

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     {
     
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs; };
+          extraSpecialArgs = {inherit inputs; };
           modules = [ 
             ./configuration.nix
             inputs.home-manager.nixosModules.default
