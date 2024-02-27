36c02a0f0007557c5dad2131fdee3a64d405ad13
borazan <boratuncel@gmail.com>
2024-02-28 00:51:01 +0100

    add pushd and popd to make script work properly

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,7 @@
 set -e
+pushd /home/borazan/nixos-config/
 alejandra . &>/dev/null
 git diff -U0 *.nix
 sudo nixos-rebuild switch --flake /home/borazan/nixos-config#default &>nixos-switch.log || (
  cat nixos-switch.log | grep --color error && false)
+ popd
