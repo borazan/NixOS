ea22bbbf92f1868bece43474320f585e871689fc
borazan <boratuncel@gmail.com>
2024-02-28 19:32:51 +0100

    fuck up again

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 set -e
-pushd /home/borazan/nixos-config/
-alejandra . &>/dev/null
+pushd /home/borazan/nixos-config
 git diff -U0 *.nix
 sudo nixos-rebuild switch --flake /home/borazan/nixos-config#default &>nixos-switch.log || (
  cat nixos-switch.log | grep --color error && false)
+echo "rebuild succesful"
 popd
